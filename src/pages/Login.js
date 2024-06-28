b457017806540fc86415645ee64932a7971a30db
Sumeet Singh <sumeet.singhji@outlook.com>
2024-06-28 12:17:25 +1000

    v1.0

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -18,6 +18,12 @@ const Login = () => {
                     <p>
                         Login to your account below
                     </p>
+                    <p>NOTE: Because this is a Learning Project and not a real/live business the Database
+                        which would normally be an AWS RDS DB is turned off to save money. 
+                        Possibly in the future a Lambda for start/stopping DB will be triggered by LogOn/Token expire/Logoff
+                        but for now for a demonstration on how it would act you can see a local server code here:
+                        <a href = "https://github.com/SumeetSinghJi/agnisamooh.com/blob/main/src/components/backend/Server.js">https://github.com/SumeetSinghJi/agnisamooh.com/blob/main/src/components/backend/Server.js</a>
+                    </p>
                     <LoginForm />
                 </div>
                 <div className="column3">
